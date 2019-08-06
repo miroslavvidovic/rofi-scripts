a239e0bbeec4d92357507eb56d9bde70a17b8ef1
Ritesh Sahu <riteshsahu314@gmail.com>
2019-08-06 16:42:22 +0000

    Fix #3 Getting error in books-search.sh

--- books-search/books-search.sh
+++ books-search/books-search.sh
@@ -19,6 +19,7 @@
 
 # Books directory
 BOOKS_DIR=~/Books
+mkdir -p ~/Books
 
 # Save find result to F_ARRAY
 readarray -t F_ARRAY <<< "$(find "$BOOKS_DIR" -type f -name '*.pdf')"
@@ -31,26 +32,40 @@ declare -A BOOKS
 
 # Add elements to BOOKS array
 get_books() {
+
+  # if [ ${#F_ARRAY[@]} != 0 ]; then
+  if [[ ! -z ${F_ARRAY[@]} ]]; then
     for i in "${!F_ARRAY[@]}"
     do
-        path=${F_ARRAY[$i]}
-        file=$(basename "${F_ARRAY[$i]}")
-        BOOKS+=(["$file"]="$path")
+      path=${F_ARRAY[$i]}
+      file=$(basename "${F_ARRAY[$i]}")
+      BOOKS+=(["$file"]="$path")
     done
+  else
+      echo "$BOOKS_DIR is empty!"
+      echo "Please put some books in it."
+      echo "Only .pdf files are accepted."
+      exit
+  fi
+
+  
 }
 
 # List for rofi
 gen_list(){
-    for i in "${!BOOKS[@]}"
-    do
-      echo "$i"
-    done
+  for i in "${!BOOKS[@]}"
+  do
+    echo "$i"
+  done
 }
 
 main() {
   get_books
   book=$( (gen_list) | rofi -dmenu -i -matching fuzzy -no-custom -location 0 -p "Book > " )
-  xdg-open "${BOOKS[$book]}"
+
+  if [ -n "$book" ]; then
+    xdg-open "${BOOKS[$book]}"
+  fi
 }
 
 main
